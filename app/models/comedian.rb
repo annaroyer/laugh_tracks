8fb6b738008467ec57b54d4b00e83e7bee67e97e
Anna Royer <anna.royer@colorado.edu>
2018-01-26 10:27:22 -0700

    Add comedians view and path and average age of comedians

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,3 +1,5 @@
 class Comedian < ActiveRecord::Base
-
+  def self.average_age
+    average(:age).round(2)
+  end
 end
---
+++ spec/features/user_sees_all_comedians_spec.rb
@@ -0,0 +1,26 @@
+describe "As a user" do
+  context "when I visit /comedians" do
+    it "I can see all the comedians" do
+      Comedian.create(name: 'funny guy', age: 20)
+
+      visit '/comedians'
+
+      save_and_open_page
+
+      expect(page).to have_content('funny guy')
+      expect(page).to have_content(20)
+    end
+  end
+end
+
+# As a user, when I visit /comedians I see a list of comedians with the following information for each comedian.
+#
+# Name
+# Age
+# As a user, when I visit /comedians I also see a list of each comedian's specials.
+#
+# As a user, when I visit /comedians I also see the average age for all comedians.
+#
+# As a user, when I visit /comedians?age=34 I see a list of all comedians with an age of 34.
+#
+# As a user, when I visit /comedians I also see the count of specials for each comedian.
