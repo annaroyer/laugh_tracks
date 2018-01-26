25b1119943528b3fc7ed21aa225dcd96dfcbb107
Anna Royer <anna.royer@colorado.edu>
2018-01-26 10:40:40 -0700

    Add specials count to each comedian

--- app/models/comedian.rb
+++ app/models/comedian.rb
@@ -1,7 +1,11 @@
 class Comedian < ActiveRecord::Base
   has_many :specials
-  
+
   def self.average_age
     average(:age).round(2)
+  end
+
+  def specials_count
+    self.specials.count
   end
 end
---
+++ spec/models/comedian_spec.rb
@@ -0,0 +1,20 @@
+describe Comedian do
+  describe "Class methods" do
+    describe ".average_age" do
+      it "returns the average age of all comedians" do
+        Comedian.create(name "Bob", age: 30)
+        Comedian.create(name "Jill", age:45)
+        Comedian.create(name "Sarah", age: 25)
+
+        expect(Comedian.average_age).to eq(33.33)
+      end
+    end
+    describe "Instance Methods" do
+      describe "#specials_count" do
+        it "returns the comedian's number of specials" do
+          
+        end
+      end
+    end
+  end
+end
